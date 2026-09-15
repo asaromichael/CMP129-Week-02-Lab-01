b449a2ee47aa3e5fc891970d02e22e3737aecc9d
asaromichael <asaro.michael@student.ccm.edu>
2026-09-15 16:54:58 -0400

    Finished KineticEnergy and started CelsiusTable

--- CelsiusTable.java
+++ CelsiusTable.java
@@ -1,3 +1,30 @@
 public class CelsiusTable {
+
+
+    public static double celsius(double F) {
+
+        double C = (5.0/9.0) * (F-32);
+        return C;
+
+
+
+    } //end of celsius
+
+
+    public static void main(String[] args) {
+
+
+        for(int count = 0; count < 21; count++) {
+
+
+            double cFinal = celsius(count);
+            System.out.print("\n" + count + "       " + cFinal);
+
+
+        } //end of for
+
+
+    } //end of main
+
     
-}
+} //end of CelsiusTable
--- KineticEnergy.java
+++ KineticEnergy.java
@@ -1,3 +1,34 @@
+import java.util.Scanner;
 public class KineticEnergy {
 
-}
+    public static double kineticEnergy(double mass, double velocity) {
+
+        double velocity2 = Math.pow(velocity, 2);
+        double kineticFinal = 0.5 * velocity2 * mass;
+
+        return kineticFinal;
+
+    } //end of kineticEnergy
+
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter the mass of the object.");
+        double massIn = scanner.nextDouble();
+        
+        System.out.println("Enter the velocity of the object.");
+        double velocityIn = scanner.nextDouble();
+
+        double kineticFinal2 = kineticEnergy(massIn, velocityIn);
+
+        System.out.print("The kinetic energy of the object is: ");
+        System.out.printf("%.2f", kineticFinal2);
+
+
+
+    } //end of main
+
+
+} //end of KineticEnergy
