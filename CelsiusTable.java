4fc17d082c9615a9e5fb87bfe15b28e1f86519d7
asaromichael <asaro.michael@student.ccm.edu>
2026-09-15 21:46:04 -0400

    Finished CelsiusTable, FutureValue and AI Report

--- CelsiusTable.java
+++ CelsiusTable.java
@@ -13,12 +13,15 @@ public class CelsiusTable {
 
     public static void main(String[] args) {
 
-
+        System.out.println("Faren.  Celsius");
+        System.out.println("---------------");
+        
         for(int count = 0; count < 21; count++) {
 
 
             double cFinal = celsius(count);
-            System.out.print("\n" + count + "       " + cFinal);
+            System.out.print("\n" + count + "       ");
+            System.out.printf("%.2f", cFinal);
 
 
         } //end of for
--- FutureValue.java
+++ FutureValue.java
@@ -1,3 +1,42 @@
+import java.util.Scanner;
 public class FutureValue {
     
-}
+
+    public static double futureValue(double present, double interest, double months) {
+
+
+        double future1 = interest + 1;
+        double future2 = Math.pow(future1, months);
+        double futureFinal = future2 * present;
+
+        return futureFinal;
+
+
+
+    } //end of futureValue Method
+
+
+    public static void main(String[] args) {
+
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the present amount.");
+        double startingAmount = scanner.nextDouble();
+        System.out.println("Enter the interest rate as a decimal.");
+        double rate = scanner.nextDouble();
+        System.out.println("Enter the time in months.");
+        double time = scanner.nextDouble();
+
+        double futureFinal2 = futureValue(startingAmount, rate, time);
+
+        System.out.print("Your final amount is $");
+        System.out.printf("%.2f", futureFinal2);
+
+
+
+    } //end of main
+
+
+} //end of FutureValue Class
